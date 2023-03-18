3eee742c54567a7bf7b3e904b290c845fe14f01d
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-18 13:22:32 +0900

    added hero img, text style

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import HeroImg from "../assets/hero-img.png"
+
+const Hero = () => {
+  return (
+    <section className="bg-primary text-white py-10">
+        <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
+      <div className="hero-info">
+        <h1 className="text-2xl  lg:text-5xl">
+          Hi <br />I am <span className="logo">J</span>elani <br />
+          frontend developer
+        </h1>
+        <p>
+          I am proficent in React.js, React Native, Tailwind and Material UI
+          CSS.
+        </p>
+        <button>See projects</button>
+      </div>
+      <div className="hero-img">
+        <img src={HeroImg} alt="coding illustration" className="w-[-80] ml-auto" />
+      </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
