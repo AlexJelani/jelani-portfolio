7972d0bc6de8db9b5e08f17800e746f4cdcace9c
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-19 13:50:36 +0900

    added container

---
+++ src/components/Projects.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <section className="bg-primary text-white py-32">
+      <div className="container mx-auto grid md:grid-cols-2 items-center md:justify-between">
+        <div className="about-info">
+          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
+            Projects
+          </h2>
+          <p className="pb-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+            dolore nostrum nihil dicta vel earum doloribus obcaecati at numquam
+            delectus.
+          </p>
+        </div>
+      </div>
+      <div className="about-img">
+
+      </div>
+      <div className="container grid md:grid-cols-3">
+
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
