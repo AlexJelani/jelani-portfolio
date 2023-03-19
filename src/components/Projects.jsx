823017967b891951a3463a31e097a5d238a50552
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-19 18:47:31 +0900

    added project imgs

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import project1 from "../assets/friendtracker.png"
+import project2 from "../assets/reactdictionary.png"
+import project3 from "../assets/cartreact.png"
 
 const Projects = () => {
   return (
@@ -18,8 +21,16 @@ const Projects = () => {
       <div className="about-img">
 
       </div>
-      <div className="container grid md:grid-cols-3">
-
+      <div className="projects container mx-auto grid md:grid-cols-3">
+        <div href="/#some project url">
+            <img src={project1} alt="" />
+        </div>
+        <div href="/#some project url">
+            <img src={project2} alt="" />
+        </div>
+        <div href="/#some project url">
+            <img src={project3} alt="" />
+        </div>
       </div>
     </section>
   );
