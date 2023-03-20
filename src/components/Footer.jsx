34f5c9a49d17a6d6141c5dd6c29a3d0c588f62fe
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-20 12:52:42 +0900

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='py-4 text-center text-white bg-secondary' > &copy; 2023 jelanialexander all rights reserverd</div>
+  )
+}
+
+export default Footer
