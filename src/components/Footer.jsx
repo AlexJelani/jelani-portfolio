3f37ab4801b1325376c4bbe7814b4a5cd402a3a9
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-20 16:36:19 +0900

    added ids

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='py-4 text-center text-white bg-secondary' > &copy; 2023 jelanialexander all rights reserverd</div>
+    <div className='py-4 text-center text-base text-white bg-primary' > &copy; 2023 jelanialexander all rights reserved</div>
   )
 }
 
