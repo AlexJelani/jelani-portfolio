e8cd35389882c08460e6c3f7f9bf2c4d48bd737c
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-23 12:58:09 +0900

    added blur overlay

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import About from "./components/About"
-import Projects from "./components/Projects"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
+import Projectstwo from "./components/Projectstwo"
 function App() {
   return (
     <>
       <Header/>
       <Hero/>
       <About/>
-      <Projects/>
+      <Projectstwo/>
       <Contact/>
       <Footer/>
     </>
--- src/components/Projectstwo.jsx
+++ src/components/Projectstwo.jsx
@@ -11,28 +11,28 @@ const Projects = () => {
       title: "My Portfolio",
       desc: " A portfolio website built with React and Tailwind CSS ",
       live: "https://devlogg.onrender.com/",
-      code: "https://github.com/Coderamrin/devlog",
+      github: "https://github.com/Coderamrin/devlog",
     },
     {
       img: project2,
       title: "Social Media Friend Tracker",
       desc: "Social Media Friend Tracker built with React, CSS, and custom API backend for CRUD operations.",
       live: "https://uilogs.xyz/",
-      code: "https://github.com/Coderamrin/html-templates",
+      github: "https://github.com/Coderamrin/html-templates",
     },
     {
       img: project3,
       title: "A React Dictionary",
       desc: " A Full-Stack React Dictionary app with an Express backend, Tailwind UI,and data from two APIs.",
       live: "https://build-10-css-projects.netlify.app/",
-      code: "https://github.com/Coderamrin/build-10-css-projects",
+      github: "https://github.com/Coderamrin/build-10-css-projects",
     },
     {
       img: project4,
       title: "React-TypeScript Shopping Cart",
       desc: "A responsive e-commerce shopping cart app is built using React and TypeScript",
       live: "https://get-inspirred.netlify.app/",
-      code: "https://github.com/Coderamrin/get-inspired",
+      github: "https://github.com/Coderamrin/get-inspired",
     },
   ];
   return (
@@ -70,6 +70,7 @@ const Projects = () => {
                 {project.desc}
               </p>
             </div>
+
           </div>
           );
         })}
@@ -80,42 +81,27 @@ const Projects = () => {
 
 export default Projects;
 
-{/* <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3"> */}
-<div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
-    style="background-image:url('https://images.unsplash.com/photo-1621111848501-8d3634f82336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80')">
-    <div
-        class="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
-        <h2 class="mt-4 text-xl font-semibold text-white capitalize">Best website collections</h2>
-        <p class="mt-2 text-lg tracking-wider text-blue-400 uppercase ">Website</p>
-    </div>
-</div>
+<a href={project.github} target="_blank">
+<button
+  className="text-center rounded-lg px-4 py-3 m-2
+             bg-white text-gray-700 font-bold text-lg"
+>
+  Code
+</button>
+</a>
+{/* eslint-disable-next-line */}
+<a href={project.live} target="_blank">
+<button
+  className="text-center rounded-lg px-4 py-3 m-2
+             bg-white text-gray-700 font-bold text-lg"
+>
+  Live
+</button>
+</a>
 
-const ProjectGrid = ({ projects }) => (
-  <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
-    {projects.map((project, i) => (
-      <div
-        key={i}
-        className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
-        style={{ backgroundImage: `url('${project.imageUrl}')` }}
-      >
-        <div
-          className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100"
-        >
-          <h2 className="mt-4 text-xl font-semibold text-white capitalize">
-            {project.title}
-          </h2>
-          <p className="mt-2 text-lg tracking-wider text-blue-400 uppercase ">
-            {project.category}
-          </p>
-        </div>
-      </div>
-    ))}
-  </div>
-);
 
-// Example usage:
-<ProjectGrid projects={projects} />;
-This code uses the map() method to iterate over the projects array, and for each project object, it generates a <div> element using the provided HTML template. The style attribute is set dynamically based on the imageUrl property of the project object, and the text content of the <h2> and <p> elements is set based on the title and category properties, respectively.
+
+
 
 
 
