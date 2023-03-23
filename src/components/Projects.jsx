3bd4033e0b9bc3cb33d4597084da54ac3ad1f390
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-23 15:34:48 +0900

    deleted aboutimg

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -49,7 +49,6 @@ const Projects = () => {
           </p>
         </div>
       </div>
-      <div className="about-img"></div>
       <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
         {projects.map((project, i) => {
           return (
