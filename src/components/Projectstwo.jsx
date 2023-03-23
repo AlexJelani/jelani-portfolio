593e156ed435c012bc9631f003cae0deca8e2539
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-23 15:26:24 +0900

    added button overlay to background imgs

--- src/components/Projectstwo.jsx
+++ src/components/Projectstwo.jsx
@@ -55,10 +55,10 @@ const Projects = () => {
           return (
             <div
               key={i}
-              className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group"
+              className="overflow-hidden bg-cover rounded-lg cursor-pointer h-96 group "
               style={{ backgroundImage: `url('${project.img}')` }}
             >
-              <div className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100">
+              <div className="flex flex-col justify-center w-full h-full px-8 py-4 transition-opacity duration-700 opacity-0 backdrop-blur-sm bg-gray-800/60 group-hover:opacity-100 show-on-mobile">
                 <h2 className="mt-4 text-xl font-semibold text-white capitalize">
                   {project.title}
                 </h2>
