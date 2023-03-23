06a9f2091bcd5c9f6097e89f8abdffc7834cbb6d
Jelani Alexander <jelanialexander82@gmail.com>
2023-03-23 18:28:51 +0900

    added datajs

---
+++ src/data.jsx
@@ -0,0 +1,15 @@
+//  icons
+import { FaLinkedin, FiGithub, FiLinkedin } from "react-icons/fi";
+
+// social
+export const social = [
+  {
+    icon: <FiLinkedin />,
+    href: "",
+  },
+
+  {
+    icon: <FiGithub />,
+    href: "",
+  },
+];
